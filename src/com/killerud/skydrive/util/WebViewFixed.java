ed8b334e7ceea18a7c08ef3734007b5c4509f264
William Killerud <klerud@gmail.com>
2012-07-04 22:36:17 +0200

    Some controls in the web preview

---
+++ src/com/killerud/skydrive/util/WebViewFixed.java
@@ -0,0 +1,39 @@
+package com.killerud.skydrive.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.webkit.WebView;
+import com.killerud.skydrive.constants.Constants;
+
+/**
+ * Fixes the onWindowFocusChanged bug, by catching NullPointerException.
+ * https://groups.google.com/d/topic/android-developers/ktbwY2gtLKQ/discussion
+ * @author Andrew
+ *
+ */
+public class WebViewFixed extends WebView {
+
+    private static final String TAG = "WebView";
+
+    public WebViewFixed(Context context) {
+        super(context);
+    }
+
+    public WebViewFixed(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public WebViewFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        try {
+            super.onWindowFocusChanged(hasWindowFocus);
+        } catch (NullPointerException ex) {
+            Log.e(TAG, "WebView.onWindowFocusChanged", ex);
+        }
+    }
+}
