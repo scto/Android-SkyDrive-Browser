bd3729d7c94b0ade340dd6db54784ea94e7925ab
William Killerud <william@killerud.com>
2013-10-28 16:16:00 +0100

    Squashed commit of the following:
    commit c03c44b3849a50cac9505c1e1c1ebba2ddc74f8e
    Author: William Killerud <william@killerud.com>
    Date:   Mon Oct 28 16:04:59 2013 +0100
    
        Added boot_complete receiver, permission

---
+++ src/com/killerud/skydrive/OnBootBroadcastReceiver.java
@@ -0,0 +1,26 @@
+package com.killerud.skydrive;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Starts the camera observer service for automatic uploads on boot,
+ * if the preference is set to true.
+ */
+public class OnBootBroadcastReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences.getBoolean("automatic_camera_upload", false))
+        {
+            context.startService(new Intent(context, CameraObserverService.class));
+        } else
+        {
+            context.stopService(new Intent(context, CameraObserverService.class));
+        }
+    }
+}
